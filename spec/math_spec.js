3a1e1bf3ca26d6294fa9dcb04a603d8eb4e51b43
Matt Seiler <Matt Seiler>
2016-02-24 09:17:41 -0500

    Functionas added for each operator

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -2,4 +2,39 @@ describe("A simple math program", function() {
   it("can add two numbers", function() {
     expect(add(3,5)).toBe(8);
   });
+
+function add(num1, num2) {
+    return num1 + num2;              
+}
+
+});
+describe("A simple math program", function() {
+  it("can subtract two numbers", function() {
+    expect(subtract(10,5)).toBe(5);
+  });
+
+function subtract(num1, num2) {
+    return num1 - num2;              
+}
+
+});
+describe("A simple math program", function() {
+  it("can multiply two numbers", function() {
+    expect(multiply(3,5)).toBe(15);
+  });
+
+function multiply(num1, num2) {
+    return num1 * num2;              
+}
+
+});
+describe("A simple math program", function() {
+  it("can divide two numbers", function() {
+    expect(divide(10,5)).toBe(2);
+  });
+
+function divide(num1, num2) {
+    return num1 / num2;            
+}
+
 });
