7e14396d677705d44fe1540d2d20cccde07dbc7b
Matt Seiler <Matt Seiler>
2016-02-24 09:55:21 -0500

    Added tests for all operators

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,40 +1,18 @@
+
+var math = require('../math') //file location
+
 describe("A simple math program", function() {
   it("can add two numbers", function() {
-    expect(add(3,5)).toBe(8);
+    expect(math.add(3,5)).toBe(8);
   });
-
-function add(num1, num2) {
-    return num1 + num2;              
-}
-
-});
-describe("A simple math program", function() {
   it("can subtract two numbers", function() {
-    expect(subtract(10,5)).toBe(5);
+    expect(math.subtract(10,5)).toBe(5);
   });
-
-function subtract(num1, num2) {
-    return num1 - num2;              
-}
-
+  it("can add two numbers", function() {
+    expect(math.multiply(3,5)).toBe(15);
+  });  
+     it("can add two numbers", function() {
+    expect(math.divide(10,5)).toBe(2);
+  });          
 });
-describe("A simple math program", function() {
-  it("can multiply two numbers", function() {
-    expect(multiply(3,5)).toBe(15);
-  });
 
-function multiply(num1, num2) {
-    return num1 * num2;              
-}
-
-});
-describe("A simple math program", function() {
-  it("can divide two numbers", function() {
-    expect(divide(10,5)).toBe(2);
-  });
-
-function divide(num1, num2) {
-    return num1 / num2;            
-}
-
-});
